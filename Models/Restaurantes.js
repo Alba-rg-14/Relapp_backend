01c965d961b0d62d058919c56c8d259db48e52ae
alba-rg-14 <albaruizgutierrez@uma.es>
2025-01-20 23:02:12 +0100

    RestauranteSchema

---
+++ Models/Restaurantes.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+
+const RestauranteSchema = new mongoose.Schema({
+    nombre: {
+        type: String,
+        required: true
+    },
+    direccion: {
+        type: String,
+        required: true
+    },
+    latitud: {
+        type: Number,
+        required: true
+    },
+    longitud: {
+        type: Number,
+        required: true
+    },
+    telefono: {
+        type: String,
+        required: false
+    },
+    horario: {
+        type: String,
+        required: false
+    },
+    categoria: {
+        type: String,
+        required: true
+    },
+    descripcion: {
+        type: String,
+        required: true
+    },
+    imagenURL: {
+        type: String,
+        required: true
+    },
+    visitado: {
+        type: Boolean,
+        required: true,
+        default: false
+    }
+});
+
+module.exports = mongoose.model('Restaurante', RestauranteSchema);    
